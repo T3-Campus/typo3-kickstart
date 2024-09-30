782ac7e204fc0a2244a078acd717648150f0e942
Kevin Chileong Lee <40547171+slavlee@users.noreply.github.com>
2024-09-30 15:03:00 +0200

    Create ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Custom Extension',
+    'description' => 'Custom Extension Description',
+    'category' => 'template',
+    'author' => 'Kevin Chileong Lee',
+    'author_email' => 'kevin.lee@wacon.de',
+    'author_company' => 'WACON Internet GmbH',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Slavlee\\CustomPackage\\' => 'Classes'
+        ]
+    ],
+];
